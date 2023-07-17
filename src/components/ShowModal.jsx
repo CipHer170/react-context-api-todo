9ac60b74fa07fc16eb1434dd05d359bf93706f90
CipHer170 <105492413+CipHer170@users.noreply.github.com>
2023-07-17 10:16:50 +0500

    modal prof

--- src/components/ShowModal.jsx
+++ src/components/ShowModal.jsx
@@ -3,11 +3,14 @@ import "./modal.scss";
 
 function ShowModal({ content, handleShowMore, showMore }) {
   return (
-    <div className="modal">
-      <div className="modal__content">{content}</div>
-      <button onClick={handleShowMore} className="todo__single_btn_showLess">
-        {showMore ? "x" : "show more"}
-      </button>
+    <div>
+      <div className="modal__window"></div>
+      <div className="modal">
+        <div className="modal__content">{content}</div>
+        <button onClick={handleShowMore} className="todo__single_btn_showLess">
+          {showMore ? "x" : "show more"}
+        </button>
+      </div>
     </div>
   );
 }
